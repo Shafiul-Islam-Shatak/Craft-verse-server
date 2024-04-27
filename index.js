95db0b3411f3f6f9bf3f499ec040ec88d1487e16
Shatak-the-imposter <helloshatak@gmail.com>
2024-04-27 21:16:27 +0600

    env file created

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 7000;
+require('dotenv').config();
 
 
 // midleware
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('craft server is runnig');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port} port`)
+  })
+
+const user = process.env.USER;
+console.log(user);
+//   mongobd
